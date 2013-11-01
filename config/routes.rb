ab104dd570ebd5ade2bd001121beeb5135ba7658
mbmosher <mbmosher@gmail.com>
2013-10-31 20:47:06 -0700

    add welcome index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ProCon::Application.routes.draw do
+  get "welcome/index"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -12,7 +13,7 @@ ProCon::Application.routes.draw do
   resources :cons
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
